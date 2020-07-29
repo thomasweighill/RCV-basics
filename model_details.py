5b97dbf9a74a07b1b0a480b98be89d8ad2eebe07
thomasweighill <thomasweighill@New-One-1.local>
2020-07-29 13:16:32 -0400

    Added BABABA

--- model_details.py
+++ model_details.py
@@ -1,6 +1,11 @@
 import pickle
 from numpy.random import choice
 from collections import defaultdict
+import compute_winners as cw
+import numpy as np
+from itertools import permutations, product
+import random
+from vote_transfers import cincinnati_transfer
 
 def Cambridge_ballot_type(
     poc_share = 0.33,
@@ -169,3 +174,77 @@ def Cambridge_ballot_type(
         )
         poc_elected_Cambridge[scenario].append(len([x for x in winners if x[0] == 'A']))
     return poc_elected_Cambridge
+
+
+def BABABA(
+    poc_share = 0.33,
+    poc_support_for_poc_candidates = 0.7,
+    poc_support_for_white_candidates = 0.3,
+    white_support_for_white_candidates = 0.8,
+    white_support_for_poc_candidates = 0.2,
+    num_ballots = 1000,
+    num_simulations = 100,
+    seats_open = 3,
+    num_poc_candidates = 2,
+    num_white_candidates = 3,
+    scenarios_to_run = ['A', 'B', 'C', 'D']
+):
+    candidates = ['A'+str(x) for x in range(num_poc_candidates)]+['B'+str(x) for x in range(num_white_candidates)]
+    poc_candidates = [c for c in candidates if c[0]=='A']
+    white_candidates = [c for c in candidates if c[0]=='B']
+
+    def interleave(x,y):
+        '''
+        Interleaves two lists x and y
+        '''
+        x = list(x)
+        y = list(y)
+        minlength = min(len(x), len(y))
+        return [z for pair in zip(x[:minlength],y[:minlength]) for z in pair]+x[minlength:]+y[minlength:]
+
+    white_bloc_ballots = {
+        'A':[white_candidates+poc_candidates],
+        'B':[white_candidates+poc_candidates],
+        'C':[list(x)+list(y) for x,y in product(list(permutations(white_candidates)), list(permutations(poc_candidates)))],
+        'D':[list(x)+list(y) for x,y in product(list(permutations(white_candidates)), list(permutations(poc_candidates)))]
+    }
+    white_cross_ballots = {
+        'A':[interleave(poc_candidates, white_candidates)],
+        'B':[interleave(poc_candidates, white_candidates)],
+        'C':[interleave(y,x) for x,y in product(list(permutations(white_candidates)), list(permutations(poc_candidates)))],
+        'D':[interleave(y,x) for x,y in product(list(permutations(white_candidates)), list(permutations(poc_candidates)))]
+    }
+    poc_bloc_ballots = {
+        'A':[poc_candidates+white_candidates],
+        'B':[list(x)+white_candidates for x in list(permutations(poc_candidates))],
+        'C':[list(y)+list(x) for x,y in product(list(permutations(white_candidates)), list(permutations(poc_candidates)))],
+        'D':[poc_candidates+white_candidates],
+    }
+    poc_cross_ballots = {
+        'A':[interleave(white_candidates, poc_candidates)],
+        'B':[interleave(white_candidates,x) for x in list(permutations(poc_candidates))],
+        'C':[interleave(x,y) for x,y in product(list(permutations(white_candidates)), list(permutations(poc_candidates)))],
+        'D':[interleave(white_candidates, poc_candidates)],
+    }
+
+    poc_elected_bababa = {}
+    for scenario in scenarios_to_run:
+      poc_elected_bababa[scenario] = []
+      for n in range(num_simulations):
+        babababallots = []
+        #poc bloc
+        a = int(num_ballots*poc_share*poc_support_for_poc_candidates)
+        babababallots.extend(random.choices(poc_bloc_ballots[scenario],k=a))
+        #poc cross
+        a = int(num_ballots*poc_share*poc_support_for_white_candidates)
+        babababallots.extend(random.choices(poc_cross_ballots[scenario],k=a))
+        #white bloc
+        a = int(num_ballots*(1-poc_share)*white_support_for_white_candidates)
+        babababallots.extend(random.choices(white_bloc_ballots[scenario],k=a))
+        #white cross
+        a = int(num_ballots*(1-poc_share)*white_support_for_poc_candidates)
+        babababallots.extend(random.choices(white_cross_ballots[scenario],k=a))
+        #winners
+        winners = cw.rcv_run(babababallots, candidates, seats_open, cincinnati_transfer)
+        poc_elected_bababa[scenario].append(len([w for w in winners if w[0]=='A']))
+    return poc_elected_bababa
